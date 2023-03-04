6b060da67793cfe14170c14d76f8542c1c69c70f
cadudevx <ces.devx@gmail.com>
2023-03-03 22:38:17 -0300

    meu primeiro commit

---
+++ type-number.js
@@ -0,0 +1,31 @@
+//codigos com operações usando nros
+
+const firstNumber = 1;
+const secondNumber = 5;
+
+// operador
+const soma = firstNumber * secondNumber;
+console.log(soma);
+
+const nrofracao = 1.9
+const nrofracaoponto = .2
+
+const resultado = nrofracao - nrofracaoponto
+console.log(resultado)
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo);
